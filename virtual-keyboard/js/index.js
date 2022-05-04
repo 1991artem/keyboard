920c000ad201e50a2dd929aa1e9a2bbefd157604
1991artem <ar_puz@mail.ru>
2022-05-04 23:00:41 +0300

    fix:'response'

--- virtual-keyboard/js/index.js
+++ virtual-keyboard/js/index.js
@@ -51,8 +51,27 @@
 
     async function keyboard_onload (){
         try{ 
-            let response = await fetch('json/key.json');
-            console.log (response);
+            let response = await fetch('json/key.json', 
+            {
+                method: "GET", // POST, PUT, DELETE, etc.
+                headers: {
+                // значение этого заголовка обычно ставится автоматически,
+                // в зависимости от тела запроса
+                "Content-Type": "text/plain;charset=UTF-8"
+                },
+                body: undefined, // string, FormData, Blob, BufferSource или URLSearchParams
+                referrer: "about:client", // или "" для того, чтобы не послать заголовок Referer,
+                // или URL с текущего источника
+                referrerPolicy: "origin-when-cross-origin", // no-referrer, origin, same-origin...
+                mode: "cors", // same-origin, no-cors
+                credentials: "same-origin", // omit, include
+                cache: "default", // no-store, reload, no-cache, force-cache или only-if-cached
+                redirect: "follow", // manual, error
+                integrity: "", // контрольная сумма, например "sha256-abcdef1234567890"
+                keepalive: false, // true
+                signal: undefined, // AbortController, чтобы прервать запрос
+                window: window // null
+              });
               if (response.ok){
                 const EN_ARRAY = await response.json();
                 console.log (EN_ARRAY);
