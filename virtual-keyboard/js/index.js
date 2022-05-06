746e220baefeb8a6509163784f15741dfc2cba7e
1991artem <ar_puz@mail.ru>
2022-05-06 22:21:37 +0300

    fix:'key value'

--- virtual-keyboard/js/index.js
+++ virtual-keyboard/js/index.js
@@ -9,6 +9,7 @@
         }
     }
 
+
     function windowForm (){
         let body = document.getElementsByTagName ('body')[0];
         body.classList.add('body');
@@ -26,8 +27,9 @@
         footer.classList.add('footer');
         page.classList.add('page');
         text_window.classList.add('text_window');
-        text_area.classList.add('text_area')
+        text_area.classList.add('text_area');
         keyboard_area.classList.add('keyboard_area');
+        keyboard_area.setAttribute('onselectstart',"return false");
     
         body.append(wrapper);
         wrapper.append(header);
@@ -42,93 +44,67 @@
 
     windowForm();
 
-    let en_lang = true;
-    let lower_case = true;
-    let ctrl = false;
+        flag = {};
+            flag.en_lang = JSON.parse(localStorage.getItem("flag.en_lang"));
+            if (flag.en_lang == undefined) {
+                flag.en_lang = true;
+            }
+            flag.lower_case = true;
+            flag.ctrl = false;
+
+    
     const KEYBOARD = document.querySelector ('.keyboard_area');
-    const EN_LOW_WORD_ARRAY = ['§','1','2','3','4','5','6','7','8','9','0','-','=','Backspace','Tab','q','w','e','r','t','y','u','i','o','p','[',']',"EN",'CapsLock','a','s','d','f','g','h','j','k','l',';','\'','\\','Enter','Shift','`','z','x','c','v','b','n','m',',','.','/','ArrowUp','Shift','Control','Alt','Meta','Space','Meta','Alt','ArrowLeft','ArrowDown','ArrowRight'];
-    const EN_UP_WORD_ARRAY = ['±','!','@','#','$','%','^','&','*','(',')','-','=','Backspace','Tab','Q','W','E','R','T','Y','U','I','O','P','[',']',"EN",'CapsLock','A','S','D','F','G','H','J','K','L',';','\'','\\','Enter','Shift','`','Z','X','C','V','B','N','M ',',','.','/','ArrowUp','Shift','Control','Alt','Meta','Space','Meta','Alt','ArrowLeft','ArrowDown','ArrowRight'];
-    const LG_LOW_WORD_ARRAY = ['§','1','2','3','4','5','6','7','8','9','0','-','=','Backspace','Tab','й','ц','у','к','е','н','г','i','щ','з','[',']',"BY",'CapsLock','ф','ы','в','а','п','р','о','л','д',';','\'','\\','Enter','Shift','`','я','ч','с','м','и','т','ь',',','.','/','ArrowUp','Shift','Control','Alt','Meta','Space','Meta','Alt','ArrowLeft','ArrowDown','ArrowRight'];
+    const EN_LOW_WORD_ARRAY = ['§','1','2','3','4','5','6','7','8','9','0','-','=','&#5130;','&#8644;','q','w','e','r','t','y','u','i','o','p','[',']',"EN",'&#8682;','a','s','d','f','g','h','j','k','l',';','\'','\\','&#8629;','&#8679;','`','z','x','c','v','b','n','m',',','.','/','&#8593;','&#8679;','control','option','command','MacOS','command','option','&#8592;','&#8595;','&#8594;'];
+    const EN_UP_WORD_ARRAY = ['±','!','@','#','$','%','^','&','*','(',')','-','=','&#5130;','&#8644;','Q','W','E','R','T','Y','U','I','O','P','[',']',"EN",'&#8682;','A','S','D','F','G','H','J','K','L',';','\'','\\','&#8629;','&#8679;','`','Z','X','C','V','B','N','M ',',','.','/','&#8593;','&#8679;','control','option','command','MacOS','command','option','&#8592;','&#8595;','&#8594;'];
+    const LG_LOW_WORD_ARRAY = ['§','1','2','3','4','5','6','7','8','9','0','-','=','&#5130;','&#8644;','й','ц','у','к','е','н','г','i','щ','з','х','ъ',"BY",'&#8682;','ф','ы','в','а','п','р','о','л','д','ж','э','\\','&#8629;','&#8679;','`','я','ч','с','м','и','т','ь',',','.','/','&#8593;','&#8679;','control','option','command','MacOS','command','option','&#8592;','&#8595;','&#8594;'];
+    const LG_UP_WORD_ARRAY = ['>','!','"','№','%',':',',','.',';','(',')','-','=','&#5130;','&#8644;','Й','Ц','У','К','Е','Н','Г','I','Щ','З','Х','Ъ',"BY",'&#8682;','Ф','Ы','В','А','П','Р','О','Л','Д','Ж','Э','\\','&#8629;','&#8679;','`','Я','Ч','С','М','И','Т','Ь','Б','Ю','/','&#8593;','&#8679;','control','Alt','Meta','MacOS','Meta','Alt','&#8592;','&#8595;','&#8594;'];
     const NAME_ARRAY = ['Backquote','Digit1','Digit2','Digit3','Digit4','Digit5','Digit6','Digit7','Digit8','Digit9','Digit0','Minus','Equal','Backspace','Tab','KeyQ','KeyW','KeyE','KeyR','KeyT','KeyY','KeyU','KeyI','KeyO','KeyP','BracketLeft','BracketRight','lng','CapsLock','KeyA','KeyS','KeyD','KeyF','KeyG','KeyH','KeyJ','KeyK','KeyL','Semicolon','Quote','Backslash','Enter','ShiftLeft','IntlBackslash','KeyZ','KeyX','KeyC','KeyV','KeyB','KeyN','KeyM','Comma','Period','Slash','ArrowUp','ShiftRight','ControlLeft','AltLeft','MetaLeft','Space','MetaRight','AltRight','ArrowLeft','ArrowDown','ArrowRight'];
 
     keyboard_onload ();
 
     const KEY_LANG = document.querySelector ('.lng');
     const TEXTAREA = document.querySelector ('.text_area');
-    const SHIFT_LEFT = document.querySelector ('.ShiftLeft');
-    const SHIFT_RIGHT = document.querySelector ('.ShiftRight');
-    const META_RIGHT = document.querySelector ('.MetaRight');
-    const META_LEFT = document.querySelector ('.MetaLeft');
-    const ALT_RIGHT = document.querySelector ('.AltRight');
-    const ALT_LEFT = document.querySelector ('.AltLeft');
+    const SHIFT_LEFT = document.querySelector ('[name=ShiftLeft]');
+    const SHIFT_RIGHT = document.querySelector ('[name=ShiftRight]');
+    const CAPS_LOCK = document.querySelector ('[name=CapsLock]');
+    const META_RIGHT = document.querySelector ('[name=MetaRight]');
+    const META_LEFT = document.querySelector ('[name=MetaLeft]');
+    const ALT_RIGHT = document.querySelector ('[name=AltRight]');
+    const ALT_LEFT = document.querySelector ('[name=AltLeft]');
     
     function keyboard_onload (){ 
         let keyArray = [];
-        switch (en_lang){
+        switch (flag.en_lang){
             case false:{
-                keyArray = LG_LOW_WORD_ARRAY;
+                if (flag.lower_case){
+                    keyArray = LG_LOW_WORD_ARRAY;
+                } else{
+                    keyArray = LG_UP_WORD_ARRAY;
+                }
                 break;
             }
             default :{
-                keyArray = EN_LOW_WORD_ARRAY;
+                if (flag.lower_case){
+                    keyArray = EN_LOW_WORD_ARRAY;
+                } else{
+                    keyArray = EN_UP_WORD_ARRAY;
+                }
             }
         }
         for (let i = 0; i < keyArray.length; i++){
             let div = document.createElement('div'); 
             let symbol = document.createElement('p');
             div.classList.add(`word`);
-            div.classList.add(`${NAME_ARRAY[i]}`);
-            symbol.classList.add('symbol'); 
-            div.setAttribute('value_low_en', keyArray[i]);
+            div.setAttribute('name', NAME_ARRAY[i]);
+            div.setAttribute('value', keyArray[i]);
+            symbol.classList.add('symbol');   
             KEYBOARD.append(div);
             div.append(symbol);
-            switch (keyArray[i]){
-                case 'Backspace' :{
-                    symbol.innerHTML = "&#5130;";
-                    break;
-                }
-                case 'Enter' :{
-                    symbol.innerHTML ="&#8629;";
-                    break;
-                }
-                case 'Shift' :{
-                    symbol.innerHTML = keyArray[i];
-                    div.setAttribute('lower_case', true);
-                    break;
-                }
-                case 'Tab' :{
-                    symbol.innerHTML = "&#8644;";
-                    break;
-                }
-                case 'CapsLock' :{
-                    symbol.innerHTML = "&#8682;";
-                    break;
-                }
-                case 'ArrowUp' :{
-                    symbol.innerHTML = "&#8593;";
-                    break;
-                }
-                case 'ArrowLeft' :{
-                    symbol.innerHTML = "&#8592;";
-                    break;
-                }
-                case 'ArrowDown' :{
-                    symbol.innerHTML = "&#8595;";
-                    break;
-                }
-                case 'ArrowRight' :{
-                    symbol.innerHTML = "&#8594;";
-                    break;
-                }
-                default :{
                     if (keyArray[i].length == 1){
                         symbol.innerHTML = keyArray[i].toUpperCase();
                     } else{
                         symbol.innerHTML = keyArray[i];
                     }
-                    
-                }
-            } 
     };
 }
 
@@ -137,7 +113,11 @@
         TEXTAREA.addEventListener('keydown', (e) => {
             try{
             if (!e.repeat){
-                document.querySelectorAll(`.${e.code}`)[0].classList.add('touch');
+                if (e.code == 'Tab') {
+                    e.preventDefault();
+                } else{
+                    document.querySelectorAll(`[name=${e.code}]`)[0].classList.add('touch');
+                }
             }
             } catch (error){
             console.log (`Press key ${e.code} + ${error}`);
@@ -145,7 +125,7 @@
           });
           TEXTAREA.addEventListener('keyup', (e) => {
             try{
-                document.querySelectorAll(`.${e.code}`)[0].classList.remove('touch');
+                document.querySelectorAll(`[name=${e.code}]`)[0].classList.remove('touch');
             } catch (error){
                 console.log (`Press key ${e.code} + ${error}`);
             }
@@ -157,66 +137,81 @@
     function text_write (){
         KEYBOARD.addEventListener('mousedown', (e) => {
             e.target.classList.add('touch');
-                switch (e.target.attributes.value_low_en.nodeValue){
+                switch (e.target.attributes.name.nodeValue){
                     case  'Space' : {
-                        TEXTAREA.value += " ";
+                        if (flag.ctrl){
+                            if (flag.en_lang == true){
+                                flag.en_lang = false;
+                                KEYBOARD.innerHTML = "";
+                                keyboard_onload ();
+                                flag.lower_case = true;
+                                flag.ctrl = false;
+                            } else {
+                                flag.en_lang = true;
+                                KEYBOARD.innerHTML = "";
+                                keyboard_onload ();
+                                flag.lower_case = true;
+                                flag.ctrl = false;
+                            }
+                            localStorage.setItem("flag.en_lang",flag.en_lang);
+                        } else {
+                            TEXTAREA.value += " ";
+                        }
                         break;
                     }
-                    case  'Shift': {
-                        e.target.classList.toggle('long_touch');
-                        if (lower_case == true){
-                            lower_case = false;
-                        } else{
-                            lower_case = true;
-                        }
-                        
+                    case  'Enter' : {
+                        TEXTAREA.value += "\n";
                         break;
                     }
-                    case  'CapsLock': {
-                        e.target.classList.toggle('long_touch');
-                        if (lower_case == true){
-                            lower_case = false;
+                    case  'Tab' : {
+                        TEXTAREA.value += '    ';
+                        break;
+                    }
+                    case  'ShiftLeft':
+                    case  'ShiftRight':
+                    case  'CapsLock': {                                       
+                        if (flag.lower_case){
+                            flag.lower_case = false;
                         } else{
-                            lower_case = true;
+                            flag.lower_case = true;
                         }
-                        
+                        KEYBOARD.innerHTML = "";
+                        keyboard_onload ();
+                        flag.ctrl = false;
                         break;
                     }
                     case  'ControlLeft': {
                         e.target.classList.toggle('long_touch');
-                        if (ctrl == false){
-                            ctrl = true;
+                        if (flag.ctrl){
+                            flag.ctrl = false;
                         } else{
-                            ctrl = false;
+                            flag.ctrl = true;
                         }
                         
                         break;
                     }
-                    case  'EN': {
-                        e.target.classList.toggle('long_touch');
-                        en_lang = false;
+                    case  'lng': {
+                        if(flag.en_lang){
+                            flag.en_lang = false;
+                        } else{
+                            flag.en_lang = true;
+                        }
                         KEYBOARD.innerHTML = "";
                         keyboard_onload ();
-                        
+                        lower_case = true;
+                        flag.ctrl = false;
+                        localStorage.setItem("flag.en_lang",flag.en_lang);
                         break;
                     }
-                    case  'BY': {
-                        e.target.classList.toggle('long_touch');
-                        en_lang = true;
-                        KEYBOARD.innerHTML = "";
-                        keyboard_onload ();
-                        
-                        break;
-                    }
-                    case  'Backspace' : {
+                    case  'Backspace' : {                                                                    
                         TEXTAREA.value =TEXTAREA.value.split('',TEXTAREA.value.length-1).join('');
                         break;
                     }
                     default : {
-                        if (e.shiftKey || lower_case == false){
-                            TEXTAREA.value += e.target.attributes.value_low_en.nodeValue.toUpperCase();
+                        if (e.shiftKey || flag.lower_case == false){
+                            TEXTAREA.value += e.target.attributes.value.nodeValue.toUpperCase();
                         } else{
-                            TEXTAREA.value += e.target.attributes.value_low_en.nodeValue;
+                            TEXTAREA.value += e.target.attributes.value.nodeValue;
                         }
                         
                     break;
